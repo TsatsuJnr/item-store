05f17d5ad324d14925d739a8fc034c0702a8c622
Tsatsu Adogla-Bessa Jnr <tsatsua@itconsortiumgh.com>
2019-07-31 11:54:23 +0000

    Database config URL variable changed

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from resources.store import Store, Stores
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABSE_URL', 'sqlite:///data.db')
+    'DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'password'
 api = Api(app)
